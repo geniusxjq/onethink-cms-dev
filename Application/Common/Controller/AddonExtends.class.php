defcdb11bff51ddbe8f1767e27476a3e3157d29e
geniusxjq <836692464@qq.com>
2015-02-14 22:59:35 +0800

    hooksInfodToArray 改为 更灵活多用点 dataToArrayList

--- Application/Common/Controller/AddonExtends.class.php
+++ Application/Common/Controller/AddonExtends.class.php
@@ -93,20 +93,47 @@ abstract class AddonExtends{
 	  } 
 	  
    /*
-	 将钩子配置信息转换为二维数组
-	 @param $info multiType 要转换的数据；
-	 @return array(array($key=>$vaule),array($key=>$vaule)...);
+	 将数据转换为二维数组
+	 @param $data multiType 要转换的数据；
+	 @param $array_temp array 返回数组样本（要求返回数组中包含的，键值）；
+	 @param $list_delimiter string  字符串转二维数组（ArrayList）时，使用的分隔符（只在'$data'类型为'string'时有效）；
+	 @param $value_delimiter string  字符串转二维数组（ArrayList）后，每个单组（List[]）内的值使用的分隔符（只在'$data'类型为'string'时有效）；
+	 @return array(array($key=>$vaule),array($key=>$vaule)...); //返回二维数组'ArrayList'；
+	 
+	 @author geniusxjq <app880.com>
+	 
+	 示例：
+	 
+	 ['$data'类型为'string']：
+	 
+	 $data="name:type:description,name:type:description";
+	
+	 $array_temp=array(
+				
+		'name'=>'',
+		
+		'type'=>'',
+		
+		'description'=>'',
+			  				
+	);
+	 
+	 调用 InfoToArrayList($data,$array_temp,",",":");  那么调用时就会按分隔符，解析成二维数组'ArrayList'。
+	 
    */
    
-	public function hooksInfoToArray($info){
+	public function dataToArrayList($data,$array_temp=null,$list_delimiter=",",$value_delimiter=":"){
 		
 		$result=array();
 		
-		if($info){
+		if($data){
 			
-			$temp_arr=array(
+			//默认返回样本
+			$array_temp=$array_temp?$array_temp:array(
 				
 			  'name'=>'',
+			  
+			  'title'=>'',
 			  
 			  'type'=>'',
 			  
@@ -114,15 +141,13 @@ abstract class AddonExtends{
 			  				
 			);
 			
-			if(is_array($info)){
-				
-				$hooks=$info;
-				
-				foreach($hooks as $key=>$value){
+			if(is_array($data)){
+								
+				foreach($data as $key=>$value){
 					
 					if(is_string($key)){
 						
-						$result[]=array_merge($temp_arr,$hooks);
+						$result[]=array_intersect_key(array_merge($array_temp,$data),$array_temp);
 						
 						break;
 						
@@ -130,7 +155,7 @@ abstract class AddonExtends{
 					
 					if(is_int($key)&&is_array($value)){
 						
-						$result[]=array_merge($temp_arr,$value);
+						$result[]=array_intersect_key(array_merge($array_temp,$value),$array_temp);
 						
 						continue;
 						
@@ -140,15 +165,27 @@ abstract class AddonExtends{
 				
 			}else{
 				
-				$hooks=explode(',',$info);
+				$data=explode($list_delimiter,$data);
 					
-				foreach($hooks as $hook_name){
+				foreach($data as $sub_value){
 					
 					$_arr=array();
-														
-					list($_arr['name'],$_arr['type'],$_arr['description'])=explode(':',$hook_name);
 					
-					$result[]=array_merge($temp_arr,$_arr);
+					$param_arr=explode($value_delimiter,$sub_value);
+					
+					$i=0;
+					
+					foreach($array_temp as $key=>$value){
+														
+					   $_arr[$key]=$param_arr[$i];
+					   
+					   $i++;
+					   
+					   continue;
+					
+					}
+					
+					$result[]=array_merge($array_temp,$_arr);
 					
 					continue;
 																
@@ -268,7 +305,7 @@ abstract class AddonExtends{
 		/* 先判断插件需要的钩子是否存在 */
 		if($install_info['hooks']){
 			
-			$hooks=$this->hooksInfoToArray($install_info['hooks']);
+			$hooks=$this->dataToArrayList($install_info['hooks'],array('name'=>'','type'=>'','description'=>''));
 						
 			foreach($hooks as $key=>$value){
 				
@@ -317,7 +354,7 @@ abstract class AddonExtends{
 		//删除钩子
 		if($install_info['hooks']){
 			
-			$hooks=$this->hooksInfoToArray($install_info['hooks']);
+			$hooks=$this->dataToArrayList($install_info['hooks'],array('name'=>'','type'=>'','description'=>''));
 			
 			foreach($hooks as $key=>$value){
 															
